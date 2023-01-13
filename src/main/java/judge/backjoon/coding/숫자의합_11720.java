2e635c75888235725f2b52ae8399ec8bcab92609
pscheol <pscheol@gmail.com>
2023-01-14 00:54:37 +0900

    init

---
+++ src/main/java/judge/backjoon/coding/숫자의합_11720.java
@@ -0,0 +1,21 @@
+package judge.backjoon.coding;
+
+import java.util.Scanner;
+
+/**
+ * https://www.acmicpc.net/problem/11720
+ */
+public class 숫자의합_11720 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int answer = 0;
+        int line = scan.nextInt();
+        String nStr = scan.next();
+        if (line == nStr.length()) {
+            for (char c : nStr.toCharArray()) {
+                answer += (c - '0');
+            }
+        }
+        System.out.println(answer);
+    }
+}
